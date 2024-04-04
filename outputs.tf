81843c508fe47fd2f3811f104e8220bc39d25437
youonmyown <1337onmyown@gmail.com>
2024-04-04 11:53:35 +0300

    add cloudfront output

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,6 @@
 output "s3_bucket_id" {
   value = aws_s3_bucket_website_configuration.website_config.website_endpoint
 }
+output "cloudfront_domain_name" {
+  value = aws_cloudfront_distribution.distribution.domain_name
+}
