c190984b282cc1ad6bfff970c8021db832be8b28
youonmyown <1337onmyown@gmail.com>
2024-04-06 13:03:10 +0300

    add manual destroy

--- cloudfront.tf
+++ cloudfront.tf
@@ -1,38 +1,38 @@
-resource "aws_cloudfront_distribution" "distribution" {
-  enabled         = true
-  is_ipv6_enabled = true
+# resource "aws_cloudfront_distribution" "distribution" {
+#   enabled         = true
+#   is_ipv6_enabled = true
 
-  origin {
-    domain_name = aws_s3_bucket_website_configuration.website_config.website_endpoint
-    origin_id   = lower(aws_s3_bucket.website_bucket.bucket)
+#   origin {
+#     domain_name = aws_s3_bucket_website_configuration.website_config.website_endpoint
+#     origin_id   = lower(aws_s3_bucket.website_bucket.bucket)
 
-    custom_origin_config {
-      http_port                = 80
-      https_port               = 443
-      origin_keepalive_timeout = 5
-      origin_protocol_policy   = "http-only"
-      origin_read_timeout      = 30
-      origin_ssl_protocols     = ["TLSv1.2"]
-    }
-  }
+#     custom_origin_config {
+#       http_port                = 80
+#       https_port               = 443
+#       origin_keepalive_timeout = 5
+#       origin_protocol_policy   = "http-only"
+#       origin_read_timeout      = 30
+#       origin_ssl_protocols     = ["TLSv1.2"]
+#     }
+#   }
 
-  viewer_certificate {
-    cloudfront_default_certificate = true
-  }
+#   viewer_certificate {
+#     cloudfront_default_certificate = true
+#   }
 
-  restrictions {
-    geo_restriction {
-      restriction_type = "none"
-      locations        = []
-    }
-  }
+#   restrictions {
+#     geo_restriction {
+#       restriction_type = "none"
+#       locations        = []
+#     }
+#   }
 
-  default_cache_behavior {
-    cache_policy_id        = "658327ea-f89d-4fab-a63d-7e88639e58f6"
-    viewer_protocol_policy = "redirect-to-https"
-    compress               = true
-    allowed_methods        = ["DELETE", "GET", "HEAD", "OPTIONS", "PATCH", "POST", "PUT"]
-    cached_methods         = ["GET", "HEAD"]
-    target_origin_id       = lower(aws_s3_bucket.website_bucket.bucket)
-  }
-}
+#   default_cache_behavior {
+#     cache_policy_id        = "658327ea-f89d-4fab-a63d-7e88639e58f6"
+#     viewer_protocol_policy = "redirect-to-https"
+#     compress               = true
+#     allowed_methods        = ["DELETE", "GET", "HEAD", "OPTIONS", "PATCH", "POST", "PUT"]
+#     cached_methods         = ["GET", "HEAD"]
+#     target_origin_id       = lower(aws_s3_bucket.website_bucket.bucket)
+#   }
+# }
